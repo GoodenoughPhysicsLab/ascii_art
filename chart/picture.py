ea8d99ae6820e82678d6ed5d5e635e148dce99c7
Arendelle <2381642961@qq.com>
2024-06-15 19:48:35 +0800

    add displayGrayVedio

--- chart/picture.py
+++ chart/picture.py
@@ -1,9 +1,38 @@
 import os
 
-from typing import Union, Optional
+from typing import Union, Optional, Callable
 
 import cv2 as cv
 import numpy as np
+
+''' chr(0x0020): space
+    chr(0x2591): 1/4 block
+    chr(0x2592): 1/2 block
+    chr(0x2593): 3/4 block
+    chr(0x2588): full block
+'''
+CHAR_PIXEL = [chr(0x0020), chr(0x2591), chr(0x2592), chr(0x2593), chr(0x2588)]
+
+def grayConvert(img: np.ndarray, multiple: Union[int, float] = 1, callback: Optional[Callable] = None):
+    if not isinstance(img, np.ndarray) or not isinstance(img[0, 0], np.uint8) or not isinstance(multiple, (int, float)):
+        raise TypeError
+
+    h, w = img.shape
+    for y in range(h):
+        for x in range(w):
+            pixel = img[y, x]
+            if pixel < 32: # 255 / 8
+                img[y, x] = 0
+            elif pixel < 96: # 255 / 8 * 3
+                img[y, x] = 1 * multiple
+            elif pixel < 160:
+                img[y, x] = 2 * multiple
+            elif pixel < 224:
+                img[y, x] = 3 * multiple
+            else:
+                img[y, x] = 4 * multiple
+            if callback is not None:
+                callback(x, y, img[y, x])
 
 def displayGrayPicture(img_path: Union[str, np.ndarray],
                        win_output: bool = False,
@@ -17,50 +46,28 @@ def displayGrayPicture(img_path: Union[str, np.ndarray],
     if isinstance(img_path, str):
         img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
     else:
-        if not isinstance(img_path[0, 0], np.uint8):
-            raise TypeError
         img = img_path
 
-    ''' chr(0x0020): space
-        chr(0x2591): 1/4 block
-        chr(0x2592): 1/2 block
-        chr(0x2593): 3/4 block
-        chr(0x2588): full block
-    '''
-    CHAR_PIXEL = [chr(0x0020), chr(0x2591), chr(0x2592), chr(0x2593), chr(0x2588)]
-    img = cv.resize(img, (img.shape[1], int(img.shape[0] / 2)))
-    h, w = img.shape
-
-    for y in range(h):
-        for x in range(w):
-            pixel = img[y, x]
-            if pixel < 32: # 255 / 8
-                img[y, x] = 0
-            elif pixel < 96: # 255 / 8 * 3
-                img[y, x] = 1
-            elif pixel < 160:
-                img[y, x] = 2
-            elif pixel < 224:
-                img[y, x] = 3
-            else:
-                img[y, x] = 4
-
     if win_output:
-        for y in range(h):
-            for x in range(w):
-                img[y, x] = int(img[y, x] * 63.75)
+        grayConvert(img, 63.75)
 
         cv.imshow("output", img)
         cv.waitKey(0)
         cv.destroyAllWindows()
     else:
-        for y in range(h):
-            for x in range(w):
-                if invert:
-                    print(CHAR_PIXEL[img[y, x]], end='')
-                else:
-                    print(CHAR_PIXEL[len(CHAR_PIXEL) - 1 - img[y, x]], end='')
-            print()
+        cache: str = ""
+        img = cv.resize(img, (img.shape[1], int(img.shape[0] / 2)))
+
+        def callback(x: int, y: int, pixel: int):
+            nonlocal cache
+            if x == 0:
+                cache += "\n"
+            if invert:
+                cache += CHAR_PIXEL[img[y, x]]
+            else:
+                cache += CHAR_PIXEL[len(CHAR_PIXEL) - 1 - img[y, x]]
+        grayConvert(img, callback=callback)
+        print(cache)
 
 def displayColorPicture(img_path: Union[str, np.ndarray], win_output: bool = False):
     if isinstance(img_path, str) and not os.path.exists(img_path):
--- chart/video.py
+++ chart/video.py
@@ -1,5 +1,64 @@
-def displayGrayVideo(video_path: str, win_output: bool = False):
-    pass
+import os
+import platform
+
+import cv2 as cv
+import chart.picture as picture
+
+from typing import Callable, Optional
+
+def displayGrayVideo(video_path: str, win_output: bool = False, invert: bool = False, frame_callback: Optional[Callable] = None):
+    if not isinstance(video_path, str) or not isinstance(win_output, bool):
+        raise TypeError("video_path must be a string")
+    if not os.path.exists(video_path):
+        raise FileNotFoundError
+
+    video = cv.VideoCapture(video_path)
+    f, frame = video.read()
+
+    if platform.system() == "Windows":
+        CLEAR = "cls"
+    else:
+        CLEAR = "clear"
+
+    try:
+        if win_output:
+            while f:
+                f, frame = video.read()
+                frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+                if frame_callback is not None:
+                    frame_callback(frame)
+
+                picture.grayConvert(frame, 63.75)
+                cv.imshow("video", frame)
+                if cv.waitKey(1) & 0xFF == ord('q'):
+                    return
+        else:
+            while f:
+                f, frame = video.read()
+                frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+                if frame_callback is not None:
+                    frame_callback(frame)
+
+                cache: str = ""
+                frame = cv.resize(frame, (frame.shape[1], int(frame.shape[0] / 2)))
+
+                def callback(x: int, y: int, pixel: int):
+                    nonlocal cache
+                    if x == 0:
+                        cache += "\n"
+                    if invert:
+                        cache += picture.CHAR_PIXEL[pixel]
+                    else:
+                        cache += picture.CHAR_PIXEL[len(picture.CHAR_PIXEL) - 1 - pixel]
+                picture.grayConvert(frame, callback=callback)
+
+                os.system(CLEAR)
+                print(cache)
+    except KeyboardInterrupt:
+        video.release()
+        cv.destroyAllWindows()
 
 def displayColorVideo(video_path: str, win_output: bool = False):
     pass
